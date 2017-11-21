c8b04540effffbe9086c16f21bb1d905458e6dda
HarukaKajita <kajitaj63b3@gmail.com>
2017-11-21 16:17:00 +0900

    カメラ切り替えの簡易実装、被写体の簡易実装

---
+++ Assets/CameraSwitcher.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour {
+
+	[SerializeField] private Camera[] cameras;
+
+	void Start () {
+		for(int i = 0; i < cameras.Length; i++){
+			if (i == 0) {
+				cameras [i].gameObject.SetActive (true);
+			} else {
+				cameras [i].gameObject.SetActive (false);
+			}
+		}
+
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A)){
+			cameras [0].gameObject.SetActive (true);
+			cameras [1].gameObject.SetActive (false);
+			cameras [2].gameObject.SetActive (false);
+
+		}else if(Input.GetKeyDown(KeyCode.B)){
+			cameras [0].gameObject.SetActive (false);
+			cameras [1].gameObject.SetActive (true);
+			cameras [2].gameObject.SetActive (false);
+
+		}else if(Input.GetKeyDown(KeyCode.C)){
+			cameras [0].gameObject.SetActive (false);
+			cameras [1].gameObject.SetActive (false);
+			cameras [2].gameObject.SetActive (true);
+
+		}
+	}
+}
+
+/*
+SetActive か enabled どっちが軽い？
+キー入力の文字を数字にして指定したカメラだけをアクティブ化は負荷かかる？
+*/
---
+++ Assets/LinesController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinesController : MonoBehaviour {
+
+	private int linesNum;
+	private LineRenderer[] lineRenderers;
+	private List<Vector3> positions1 = new List<Vector3>();
+	private List<Vector3> positions2 = new List<Vector3>();
+	private Dictionary<string, float> seeds = new Dictionary<string, float>();
+	private Vector3 vec1, vec2;
+	[SerializeField] private Material mat;
+
+	void Start () {
+		linesNum = transform.childCount;
+		lineRenderers = GetComponentsInChildren<LineRenderer> ();
+		foreach( LineRenderer LR in lineRenderers){
+			//LR.material   = new Material(Shader.Find("Particles/Additive"));
+			LR.material = mat;
+			LR.useWorldSpace = false;
+			LR.startWidth = 0.01f;
+			LR.endWidth   = 0.01f;
+//			LR.startColor = Color.black;
+//			LR.endColor   = Color.black;
+		}
+			
+		seeds.Add ("1x",Random.value*100);
+		seeds.Add ("1y",Random.value*100);
+		seeds.Add ("1z",Random.value*100);
+		seeds.Add ("2x",Random.value*100);
+		seeds.Add ("2y",Random.value*100);
+		seeds.Add ("2z",Random.value*100);
+
+
+		vec1 = new Vector3 ( 
+			Perlin.Noise(seeds["1x"]),
+			Perlin.Noise(seeds["1y"]),
+			Perlin.Noise(seeds["1z"]));
+
+		vec2 = new Vector3 ( 
+			Perlin.Noise(seeds["2x"]),
+			Perlin.Noise(seeds["2y"]),
+			Perlin.Noise(seeds["2z"]));
+		
+		for(int i = 0; i < linesNum; i++){
+			
+			positions1.Add (vec1);
+			positions2.Add (vec2);
+			lineRenderers [i].SetPosition (0,positions1[positions1.Count-1]);
+			lineRenderers [i].SetPosition (1,positions2[positions2.Count-1]);
+
+			List<string> keys = new List<string> (seeds.Keys);
+			foreach(string key in keys){
+				seeds[key] += Random.Range(0.01f, 0.02f);
+			}
+
+			vec1 = new Vector3 ( 
+				Perlin.Noise(seeds["1x"]),
+				Perlin.Noise(seeds["1y"]),
+				Perlin.Noise(seeds["1z"]));
+
+			vec2 = new Vector3 ( 
+				Perlin.Noise(seeds["2x"]),
+				Perlin.Noise(seeds["2y"]),
+				Perlin.Noise(seeds["2z"]));
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < linesNum-1; i++){
+			positions1 [i] = positions1 [i + 1];
+			positions2 [i] = positions2 [i + 1];
+		}
+
+		positions1 [positions1.Count - 1] = vec1;
+		positions2 [positions2.Count - 1] = vec2;
+
+		List<string> keys = new List<string> (seeds.Keys);
+		foreach(string key in keys){
+			seeds[key] += Random.Range(0.01f, 0.02f);
+		}
+
+		vec1 = new Vector3 ( 
+			Perlin.Noise(seeds["1x"]),
+			Perlin.Noise(seeds["1y"]),
+			Perlin.Noise(seeds["1z"]));
+
+		vec2 = new Vector3 ( 
+			Perlin.Noise(seeds["2x"]),
+			Perlin.Noise(seeds["2y"]),
+			Perlin.Noise(seeds["2z"]));
+
+		for(int i = 0; i < linesNum; i++){
+			lineRenderers[i].SetPosition (0, positions1[i]);
+			lineRenderers[i].SetPosition (1, positions2[i]);
+
+		}
+	}
+}
