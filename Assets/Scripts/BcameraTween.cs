21422186eb1ba115f28a3dbce290cc6e43f66b16
HarukaKajita <kajitaj63b3@gmail.com>
2017-11-22 22:02:09 +0900

    カメラのアニメーションをDoTweenで設定 他調整

---
+++ Assets/Scripts/BcameraTween.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BcameraTween : MonoBehaviour {
+
+	private int pointsNum = 10;
+	public Vector3[] movePoints;
+	[SerializeField] private Transform target;
+
+
+	void OnEnable(){
+
+		movePoints = new Vector3[pointsNum];
+		for(int i = 0; i < pointsNum-1; i++){
+			var x = -6 * Mathf.Sin (i*((Mathf.PI * 2) /(pointsNum-1) ));
+			var z = -6 * Mathf.Cos (i*((Mathf.PI * 2) /(pointsNum-1) ));
+
+			movePoints [i] = new Vector3 (x,1,z);
+		}
+
+		movePoints [pointsNum-1] = new Vector3 (0,1,-6);
+
+	}
+
+	void Start (){
+	}
+	
+	void Update () {
+		transform.LookAt (target);
+	}
+}
---
+++ Assets/Scripts/CcameraTween.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CcameraTween : MonoBehaviour {
+
+	private int pointsNum = 50;
+	public Vector3[] movePoints; //Tweenの移動ルートのパス
+	[SerializeField] private Transform target;
+
+	[SerializeField] private int cycleNum = 3;
+	void OnEnable(){
+
+		movePoints = new Vector3[pointsNum];
+		for(int i = 0; i < pointsNum; i++){
+			
+			var x = -6 * Mathf.Sin (i * ((Mathf.PI * 2 * cycleNum) /(pointsNum) ));
+			var z = -6 * Mathf.Cos (i * ((Mathf.PI * 2 * cycleNum) /(pointsNum) ));
+			var y = 10 - (i * (9f / pointsNum));
+
+			movePoints [i] = new Vector3 (x,y,z);
+		}
+	}
+
+	void Start (){
+	}
+
+	void Update () {
+		transform.LookAt (target);
+	}
+}
